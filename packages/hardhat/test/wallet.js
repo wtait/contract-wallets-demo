f2b1624ccae834b0e022719d78ed31f564afdcde
wade tait <wadetait@wades-Mac-mini.local>
2022-06-24 13:27:21 -0700

    test/wallet.js cleanup

--- packages/hardhat/test/wallet.js
+++ packages/hardhat/test/wallet.js
@@ -3,14 +3,9 @@ const { expect } = require("chai");
 //using "hardhat-abi-exporter" package to publish abi to packages/hardhat/abi prior to
 //each deploy. To do this we call "hardhat clear-abi && hardhat export-abi" in the test command of package.json
 const walletABI = require("../abi/contracts/SmartWallet.json");
-//const walletABI = require("../artifacts/contracts/SmartWallet.sol/SmartWallet.json");
 const { keccak256 } = require("@ethersproject/keccak256");
 
 
-
-// let testSalt = ethers.utils.randomBytes(32)
-// testSalt = keccak256(testSalt)
-// console.log(testSalt)
 
 const getSalt = () => {
   return keccak256(ethers.utils.randomBytes(32))
@@ -26,9 +21,7 @@ const  arbitraryMsgHash =
 
 const arbitrarySalt = getSalt()
 
-  // '0x0000000000000000000000000000000000000000000000000000000000000001'; //salt method just for testing... evaluate proper implementation later
-// const arbitrarySalt2 = 
-//   '0x0000000000000000000000000000000000000000000000000000000000000002';
+
 
 const message = "a simple message";
 
@@ -101,48 +94,3 @@ describe("1271Wallet", () => {
   });
 });
 
-
-
-
-
-// const { ethers } = require("hardhat");
-// const { use, expect } = require("chai");
-// const { solidity } = require("ethereum-waffle");
-
-// use(solidity);
-
-// describe("My Dapp", function () {
-//   let myContract;
-
-//   // quick fix to let gas reporter fetch data from gas station & coinmarketcap
-//   before((done) => {
-//     setTimeout(done, 2000);
-//   });
-
-//   describe("YourContract", function () {
-//     it("Should deploy YourContract", async function () {
-//       const YourContract = await ethers.getContractFactory("YourContract");
-
-//       myContract = await YourContract.deploy();
-//     });
-
-//     describe("setPurpose()", function () {
-//       it("Should be able to set a new purpose", async function () {
-//         const newPurpose = "Test Purpose";
-
-//         await myContract.setPurpose(newPurpose);
-//         expect(await myContract.purpose()).to.equal(newPurpose);
-//       });
-
-//       it("Should emit a SetPurpose event ", async function () {
-//         const [owner] = await ethers.getSigners();
-
-//         const newPurpose = "Another Test Purpose";
-
-//         expect(await myContract.setPurpose(newPurpose))
-//           .to.emit(myContract, "SetPurpose")
-//           .withArgs(owner.address, newPurpose);
-//       });
-//     });
-//   });
-// });
