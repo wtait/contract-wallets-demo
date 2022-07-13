f70039b739ebff24a1f58eabcd48177e86d40d46
wade tait <wadetait@wades-Mac-mini.wavecable.com>
2022-07-12 22:59:10 -0700

    removes comments from Account component

--- packages/react-app/src/components/Account.tsx
+++ packages/react-app/src/components/Account.tsx
@@ -8,12 +8,10 @@ import Fingerprint from '@mui/icons-material/Fingerprint';
 const Account = () => 
 
    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', alignItems: 'center' }}>
-    {/* <ButtonGroup variant="outlined"> */}
       <IconButton aria-label="fingerprint" variant="outlined" sx={{color: 'black', borderColor: 'black'}}>
         <Fingerprint />
       </IconButton>
       <Button variant="outlined" sx={{color: 'black', borderColor: 'black'}}>Connect</Button>
-    {/* </ButtonGroup> */}
    </Box>
 
 
