2a7e7820f3301e42a4173868d683eba7ab6c3b63
wade tait <wadetait@wades-Mac-mini.wavecable.com>
2022-07-12 18:28:29 -0700

    format tests

--- packages/react-app/src/components/__tests__/App.test.js
+++ packages/react-app/src/components/__tests__/App.test.js
@@ -5,7 +5,6 @@ import Wallet from "../Wallet";
 
 it('renders the account component', () => {
     const div = document.createElement('div');
-
     ReactDOM.render(<App />, div);
     expect(div).toContainElement(Wallet)
     ReactDOM.unmountComponentAtNode(div);
