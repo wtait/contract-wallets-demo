59fdc43449752770207e3fd47a173d5a39cf2bdc
Wade <wade@cgnetworks.dev>
2022-07-13 18:20:39 -0700

    use material ui styling for connect button

--- packages/react-app/src/components/Account.tsx
+++ packages/react-app/src/components/Account.tsx
@@ -45,7 +45,83 @@ const Account = () =>
 <WagmiConfig client={wagmiClient}>
 <RainbowKitProvider chains={chains} theme={darkTheme()}>
    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap', alignItems: 'center' }}>
-      <ConnectButton/>
+   <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        mounted,
+      }) => {
+        return (
+          <div
+            {...(!mounted && {
+              'aria-hidden': true,
+              'style': {
+                opacity: 0,
+                pointerEvents: 'none',
+                userSelect: 'none',
+              },
+            })}
+          >
+            {(() => {
+              if (!mounted || !account || !chain) {
+                return (
+                   <Button onClick={openConnectModal} variant="outlined" type="button" sx={{color:'black', borderColor:'black'}}>Connect Wallet</Button>
+                );
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <Button onClick={openConnectModal} variant="outlined" type="button" sx={{color:'black', borderColor:'black'}}>Wrong Network</Button>
+                );
+              }
+
+              return (
+                <div style={{ display: 'flex', gap: 12 }}>
+                   
+                  <Button 
+                    onClick={openChainModal} 
+                    variant="outlined" 
+                    type="button" 
+                    sx={{color:'black', borderColor:'black'}}>
+                    {chain.hasIcon && (
+                      <div
+                        style={{
+                          background: chain.iconBackground,
+                          width: 12,
+                          height: 12,
+                          borderRadius: 999,
+                          overflow: 'hidden',
+                          marginRight: 4,
+                        }}
+                      >
+                        {chain.iconUrl && (
+                          <img
+                            alt={chain.name ?? 'Chain icon'}
+                            src={chain.iconUrl}
+                            style={{ width: 12, height: 12 }}
+                          />
+                        )}
+                      </div>
+                    )}
+                    {chain.name}
+                  </Button>
+                  
+                  <Button onClick={openAccountModal} variant="outlined" type="button" sx={{color:'black', borderColor:'black'}}>
+                    {account.displayName}
+                    {account.displayBalance
+                      ? ` (${account.displayBalance})`
+                      : ''}
+                  </Button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
    </Box>
 </RainbowKitProvider>
 </WagmiConfig>
